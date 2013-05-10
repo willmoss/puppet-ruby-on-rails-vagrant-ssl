d19f555bd963f562130e644c0a06bad560700abd
William Moss <willmoss26@gmail.com>
2013-05-10 17:04:41 +0100

    deploy v2

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-#http://wildlyinaccurate.com/deploying-a-git-repository-to-a-remote-server
-
-git archive --format=tar origin/master | gzip -9c | ssh root@5.79.22.251 "tar --directory=/etc/puppet -xvzf -"
