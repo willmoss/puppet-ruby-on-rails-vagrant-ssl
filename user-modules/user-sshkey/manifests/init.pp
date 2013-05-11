e52b341293215c57b0d2d3f9d58e35c8e7f7c682
William Moss <willmoss26@gmail.com>
2013-05-11 14:01:32 +0100

    ssh key v2

--- user-modules/user-sshkey/manifests/init.pp
+++ user-modules/user-sshkey/manifests/init.pp
@@ -9,14 +9,14 @@ class user-sshkey($user,$type) {
         # in with the new
         file { "/home/$user/.ssh/id_$type":
                 ensure  => present,
-                source  => "puppet:///modules/user-sshkey/$user/id_$type",
+                source  => "puppet:///user-modules/user-sshkey/$user/id_$type",
                 owner   => $user,
                 group   => $user,
                 mode    => 0600,
         }
         file { "/home/$user/.ssh/id_$type.pub":
                 ensure => present,
-                source => "puppet:///modules/user-sshkey/$user/id_$type.pub",
+                source => "puppet:///user-modules/user-sshkey/$user/id_$type.pub",
                 owner   => $user,
                 group   => $user,
                 mode    => 0644,
@@ -27,7 +27,7 @@ class user-sshkey($user,$type) {
                 ensure  => present,
                 user    => $user,
                 type    => "ssh-$type",
-                key     => generate( "/etc/puppet/bin/extract_key", "/etc/puppet/modules/user-sshkey/files/$user/id_$type.pub" ),
+                key     => generate( "/etc/puppet/bin/extract_key", "/etc/puppet/user-modules/user-sshkey/files/$user/id_$type.pub" ),
                 require => [File["/home/$user/.ssh/id_$type.pub"]],
         }
 }
