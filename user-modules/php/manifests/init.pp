71af8364d753f9f68e5c22e0968a7195ce8a66ff
William Moss <willmoss26@gmail.com>
2013-05-25 21:22:42 -0700

    add php modules

--- user-modules/php/manifests/init.pp
+++ user-modules/php/manifests/init.pp
@@ -3,6 +3,22 @@ class php {
   package { "php5":
     ensure => present,
   }
+  
+  package { "php5-common":
+    ensure => present,
+  }
+  
+  package { "php5-dev":
+    ensure => present,
+  }
+  
+  package { "php5-cli":
+    ensure => present,
+  }
+  
+  package { "php5-mysql":
+    ensure => present,
+  }
  
   package { "libapache2-mod-php5":
     ensure => present,
