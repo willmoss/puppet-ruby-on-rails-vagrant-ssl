c81eb48e6d598c675f363a6cd0fee8fea4ca1ea8
William Moss <willmoss26@gmail.com>
2013-05-27 14:36:44 -0700

    edit pass confg

--- user-modules/passenger/manifests/init.pp
+++ user-modules/passenger/manifests/init.pp
@@ -2,12 +2,10 @@ class passenger {
 	exec {
 		"/usr/local/bin/gem install passenger -v4.0.2 --no-ri --no-rdoc":
 		  user    => root,
-	  	  group   => root,
+		  group   => root,
 		  alias   => "install_passenger",
-                  require => Package["apache2"],
-		  before  => [File["passenger_conf"],Exec["passenger_apache_module"]],
-		  #before  => [Exec["passenger_apache_module"]],
-		  unless  => "ls /usr/local/lib/ruby/gems/1.9.1/gems/passenger-*/"
+      require => Package["apache2"],
+		  unless  => "ls /usr/local/lib/ruby/gems/1.9.1/gems/passenger-4.0.2/"
 	}
 	exec {
 		"/usr/local/bin/passenger-install-apache2-module --auto":
@@ -15,15 +13,15 @@ class passenger {
 		  group   => root,
 		  path    => "/bin:/usr/bin:/usr/local/apache2/bin/",
 		  alias   => "passenger_apache_module",
-		  before  => File["passenger_conf"],
+		  require => Exec["install_passenger"],
 		  unless  => "ls /usr/local/lib/ruby/gems/1.9.1/gems/passenger-4.0.2/libout/apache2/mod_passenger.so"
 	}
 
 	package {
-          "libcurl4-openssl-dev" :
-            ensure => present,
-            before => Exec["install_passenger"]
-        }
+    "libcurl4-openssl-dev" :
+      ensure => present,
+      before => Exec["install_passenger"]
+  }
 
 	package {
 	  "apache2-prefork-dev":
@@ -50,6 +48,7 @@ class passenger {
 		  group   => root,
 		  alias   => "passenger_conf",
 		  notify  => Service["apache2"],
-		  source  => "puppet:///modules/passenger/passenger.conf"
+		  source  => "puppet:///modules/passenger/passenger.conf",
+		  require => Exec["passenger_apache_module"],
 	}
 }
